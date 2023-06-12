1a2acebcd4f6f925073a4c4be67dd0e808718f13
CrisPer1988 <norberto.cp@hotmail.com>
2023-06-11 21:46:10 -0300

    replace ol/H5

--- src/components/AcercaDeMi.jsx
+++ src/components/AcercaDeMi.jsx
@@ -15,18 +15,18 @@ const AcercaDeMi = () => {
         <h2>¿Que me describe? <br /> ¿Como soy?</h2>
         <ul>
           <li className='title__hablandas'>Responsable:</li>
-          <ol className='description__ol'>Ante todo, responsabilidad</ol>
+          <h5 className='description__ol'>Ante todo, responsabilidad</h5>
           <li className='title__hablandas'>Atento:</li>
-          <ol className='description__ol'>Pendiente a todo</ol>
+          <h5 className='description__ol'>Pendiente a todo</h5>
           <li className='title__hablandas'>Compañero:</li>
-          <ol className='description__ol'>Si el equipo no es amigo, no hay equipo</ol>
+          <h5 className='description__ol'>Si el equipo no es amigo, no hay equipo</h5>
           <li className='title__hablandas'>Puntual:</li>
-          <ol className='description__ol'>Puntualidad = Respeto</ol>
+          <h5 className='description__ol'>Puntualidad = Respeto</h5>
           <li className='title__hablandas'>Facilidad de aprendizaje:</li>
-          <ol className='description__ol'>Si alguien lo hizo, tambien puedo hacerlo</ol>
-          <ol className='description__ol'>Si nadie lo hizo, seré el primero en hacerlo</ol>
+          <h5 className='description__ol'>Si alguien lo hizo, tambien puedo hacerlo</h5>
+          <h5 className='description__ol'>Si nadie lo hizo, seré el primero en hacerlo</h5>
           <li className='title__hablandas'>Trabajo en equipo:</li>
-          <ol className='description__ol'>Lo que mas disfruto</ol>
+          <h5 className='description__ol'>Lo que mas disfruto</h5>
         </ul>
       </div>
       <div>
